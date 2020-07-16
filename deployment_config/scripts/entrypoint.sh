86bd291ce48e7ffaed49c5a891e01108dd83510d
Tuhin Sharma <tuhinsharma@Tuhins-MacBook-Pro.local>
2020-07-16 22:06:15 +0530

    fixed refresh error

--- deployment_config/scripts/entrypoint.sh
+++ deployment_config/scripts/entrypoint.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/env bash
 
 # --------------------------------------------------------------------------------------------------
-# generate key and cert for https using openssl
-# --------------------------------------------------------------------------------------------------
-
-#openssl req -newkey rsa:2048 -nodes -keyout /myserver-dev.key -x509 -out /myserver-dev.crt -subj "/C=US/ST=TEXAS/L=AUSTIN/O=IT/CN="$HOST_PUBLIC_IP_ADDR
-
-# --------------------------------------------------------------------------------------------------
-# start web service to provide rest end points for this container
+# start web service to provide rest end points for this container at 8080
 # --------------------------------------------------------------------------------------------------
 
 printenv > /.env
 npm run build
-
-pm2 serve build/
-#gunicorn --pythonpath / -b 0.0.0.0:$SERVICE_PORT -k gevent -t $SERVICE_TIMEOUT -w $WORKER_COUNT optimization_platform.deployment.server:app -k uvicorn.workers.UvicornWorker
-
-#gunicorn --certfile=myserver-dev.crt --keyfile=myserver-dev.key --pythonpath / -b 0.0.0.0:$SERVICE_PORT -k gevent -t $SERVICE_TIMEOUT -w $WORKER_COUNT optimization_platform.deployment.server:app -k uvicorn.workers.UvicornWorker
-#gunicorn --certfile=myserver-dev.crt --keyfile=myserver-dev.key --pythonpath / -b 0.0.0.0:6006 -k gevent -t 300 -w 2 optimization_platform.deployment.server:app -k uvicorn.workers.UvicornWorker
-
+pm2 serve build --spa
 
 # --------------------------------------------------------------------------------------------------
 # to make the container alive for indefinite time
@@ -26,9 +14,3 @@ pm2 serve build/
 
 touch /tmp/a.txt
 tail -f /tmp/a.txt
-
-# --------------------------------------------------------------------------------------------------
-# run directly
-# ------------------------------------------
-
-#export PYTHONPATH=`pwd`
